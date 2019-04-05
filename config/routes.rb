e9eda388e3864b896d4b4e43d590403db201f23e
Cameron Sugiada <s3664000@student.rmit.edu.au>
2019-04-05 00:46:12 +0000

    Fixed Desployment Bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     root 'static_pages#home'
-    get '/help', to: 'static_pages#help', as: 'help'
-    get '/about', to: 'static_pages#about', as: 'about'
+    get '/help', to: 'static_pages#help'
+    get '/about', to: 'static_pages#about'
     get '/contact', to: 'static_pages#contact'
 end
