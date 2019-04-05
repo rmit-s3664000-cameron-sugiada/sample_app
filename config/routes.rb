823d148e281b89c225fd757ec7700e08dca3be9e
Cameron Sugiada <s3664000@student.rmit.edu.au>
2019-04-05 00:23:44 +0000

    Tute 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-    get 'static_pages/home'
-
-    get 'static_pages/help'
-    
-    get 'static_pages/about'
-
-    root 'application#hello'
+    root 'static_pages#home'
+    get '/help', to: 'static_pages#help', as: 'help'
+    get '/about', to: 'static_pages#about', as: 'about'
+    get '/contact', to: 'static_pages#contact'
 end
